f46b9e6d89f2ac217adf7a4d200573f28aec3f16
Pooja Patil <poojabpatil2309@gmail.com>
2025-08-15 00:00:31 +0530

    Add files via upload

---
+++ text_summary.py
@@ -0,0 +1,17 @@
+import torch
+import gradio as gr
+
+from transformers import pipeline
+text_summary = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", torch_dtype=torch.bfloat16)
+
+def summary(input):
+    output=text_summary(input)
+    return output[0]['summary_text']
+gr.close_all()
+
+demo=gr.Interface(fn=summary,
+                  inputs=[gr.Textbox(label="Input text to summarize", lines=6)],
+                  outputs=[gr.Textbox(label="Summarized text", lines=4)],
+                  title="Text Summarizer",
+                  description="This application gives us the summarized text")
+demo.launch()
